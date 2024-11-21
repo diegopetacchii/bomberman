a765793a85eeb576859b3254119910c051060b8a
PetacchiDiego <diego.petacchi@studenti.iispascal.it>
2024-11-21 12:11:48 +0100

    tutto nuovo con immagini
    uela

--- bomberman.py
+++ bomberman.py
@@ -20,8 +20,95 @@ class Bomberman(Actor):
         self._countU, self._countD, self._countL, self._countR = 0, 0, 0, 0
         self._timerDeath=70
         self._death=False
+        
+    def is_alive(self):
+        return not(self._death)
+
 
     def move(self, arena: Arena):
+        if self._death:
+            self.deathAnimation(arena)
+        else:
+            keys = arena.current_keys()
+            
+            if "ArrowUp" in keys:
+                if self._countU==1:
+                    self._spriteW, self._spriteH=48, 16
+                if self._countU==2:
+                    self._spriteW, self._spriteH=64, 16
+                if self._countU==3:
+                    self._spriteW, self._spriteH=80, 16  
+                    self._countU=0
+                self._countU=self._countU+1    
+                self._dy -= self._speed
+                self._dx = 0
+            elif "ArrowDown" in keys:
+                if self._countD==1:
+                    self._spriteW, self._spriteH=48, 0
+                if self._countD==2:
+                    self._spriteW, self._spriteH=64, 0
+                if self._countD==3:
+                    self._spriteW, self._spriteH=80, 0  
+                    self._countD=0
+                self._countD=self._countD+1
+                self._dy += self._speed
+                self._dx = 0
+            elif "ArrowLeft" in keys:
+                if self._countL==1:
+                    self._spriteW, self._spriteH=0, 0
+                if self._countL==2:
+                    self._spriteW, self._spriteH=16, 0
+                if self._countL==3:
+                    self._spriteW, self._spriteH=32, 0  
+                    self._countL=0
+                self._countL=self._countL+1
+                self._dx -= self._speed
+                self._dy = 0
+                self.update_sprite("left")
+            elif "ArrowRight" in keys:
+                if self._countR==1:
+                    self._spriteW, self._spriteH=0, 16
+                if self._countR==2:
+                    self._spriteW, self._spriteH=16, 16
+                if self._countR==3:
+                    self._spriteW, self._spriteH=32, 16  
+                    self._countR=0
+                self._countR=self._countR+1
+                self._dx += self._speed
+                self._dy = 0
+
+            if "Spacebar" in keys:
+                if not any(isinstance(a, Bomb) for a in arena.actors()):
+                    arena.spawn(Bomb((self._x, self._y)))
+
+            # Controllo collisioni prima di aggiornare la posizione
+            if not self.check_collision(self._x + self._dx, self._y + self._dy, arena):
+                self._x += self._dx
+                self._y += self._dy
+
+            # Reset dx e dy dopo il movimento
+            self._dx = 0
+            self._dy = 0
+
+            # Check door collision
+        if self.check_door_collision(arena):
+            print("Camilla De Pandis SMASH")
+
+    def update_sprite(self, direction):
+        if direction == "up":
+            self._countU = (self._countU + 1) % 4
+            self._spriteW, self._spriteH = 48 + 16 * self._countU, 16
+        elif direction == "down":
+            self._countD = (self._countD + 1) % 4
+            self._spriteW, self._spriteH = 48 + 16 * self._countD, 0
+        elif direction == "left":
+            self._countL = (self._countL + 1) % 4
+            self._spriteW, self._spriteH = 0 + 16 * self._countL, 0
+        elif direction == "right":
+            self._countR = (self._countR + 1) % 4
+            self._spriteW, self._spriteH = 0 + 16 * self._countR, 16
+
+    """def move(self, arena: Arena):
         
         if self._death==True:
             self.deathAnimation(arena)
@@ -96,7 +183,7 @@ class Bomberman(Actor):
             
         
             if self.check_door_collision(arena):
-                print("Camilla De Pandis SMASH")
+                print("Camilla De Pandis SMASH")"""
     
     def check_collision(self, next_x, next_y, arena: Arena) -> bool:
         
--- main.py
+++ main.py
@@ -17,6 +17,8 @@ delta_pos=30
 timer=200
 count_frame=0
 fps=30
+left =3
+point=0
 
 def walls_distr(arena, num_walls):
     global AX2, AY2, delta_pos
@@ -51,8 +53,7 @@ def spawn_door(arena):
             print(f"Porta creata alla posizione: {(wx, wy)}") 
             break
  
-
-def tick(arena:Arena):
+def tick():
     global AX2, AY2, delta_pos, timer, count_frame, fps
     arena_width, arena_height = arena.size()
     
@@ -67,13 +68,25 @@ def tick(arena:Arena):
     if count_frame==fps:
         count_frame=0
         timer=timer-1
-    g2d.set_color((255, 105, 105))
+    
+    g2d.set_color((255, 255, 255))
     g2d.draw_text(f"Time: {timer}", (50, 15), 15)
-    if timer==195:
-        game_over(arena)
-        return
-        
+    
+    g2d.set_color((255, 255, 255))
+    g2d.draw_text(f"Points: {point}", (200, 15), 15)
 
+    for b in arena.actors():
+        if isinstance(b, Bomberman):
+            bomberman = b 
+            break 
+    else: 
+        bomberman = None
+    if bomberman.is_alive()==False or timer==0: 
+        game_over(arena) 
+        return
+    
+    g2d.set_color((255, 255, 255))
+    g2d.draw_text(f"Left: {left}", (400, 15), 15)    
     for a in arena.actors():
         if isinstance(a, Wall):
             g2d.draw_image("bomberman.png", a.pos(), a.sprite(), a.size())
@@ -96,11 +109,28 @@ def tick(arena:Arena):
     arena.tick(g2d.current_keys())
 
 
-def game_over(arena:Arena):
-    arena_width, arena_height = arena.size()
-    g2d.draw_text("GAME OVER", (arena_width//2-50, arena_height//2), 30)
+def game_over(arena: Arena): 
+    global left 
+    left = left - 1
+    if left > 0: 
+        reset() 
+    else: 
+        left=3
+        arena_width, arena_height = arena.size() 
+        g2d.set_color((0, 0, 0)) 
+        g2d.draw_rect((0, 0), (arena_width, arena_height)) 
+        g2d.set_color((255, 0, 0)) 
+        g2d.draw_text("GAME OVER", (arena_width // 2, arena_height // 2), 50) 
+        g2d.update_canvas() 
+        time.sleep(3)
+        main()
+
+def reset(): 
+    global timer, point, count_frame 
+    timer = 200
+    point = 0 
+    count_frame = 0 
     main()
-    
 
 
 def add_walls(arena):
@@ -129,36 +159,34 @@ def add_walls(arena):
     
     spawn_door(arena)
 
-def home_screen():
-    global g2d, AX, AY
+def start():
+    global g2d, arena, AX, AY
+    import g2d
+
     arena = Arena((AX, AY))
-    g2d.init_canvas((AX, AY))
+    g2d.init_canvas(arena.size())
+    g2d.draw_image("homescreen.jpeg", (0,0),(AX, AY))
+    g2d.update_canvas()
+
+    time.sleep(3)
+
+    g2d.draw_image("stage1.jpeg", (0,0),(AX, AY))
+    g2d.update_canvas()
+
+    time.sleep(3)
+    arena.spawn(Ballom((16, 64)))
+    arena.spawn(Bomberman((16, 160)))
+
+    add_walls(arena)  # Aggiungi i muri ai bordi e nelle altre posizioni
+
     
-    ciclo = True
-    while ciclo:
-        # Disegna la schermata iniziale
-        print("ciao")
-        g2d.clear_canvas()
-        g2d.draw_text("Press Enter to Start", (AX // 2 - 100, AY // 2), 24)  # Centra il testo
-        g2d.update_canvas()
+    g2d.main_loop(tick)  # Avvia il ciclo principale e assicurati che il canvas si aggiorni
 
-        time.sleep(1) 
-
-        # Controlla i tasti premuti
-        keys = g2d.current_keys()
-        if "Enter" in keys:
-            print("Enter premuto")
-            arena.spawn(Ballom((16, 128)))
-            arena.spawn(Bomberman((16, 288)))
-            add_walls(arena)
-            g2d.main_loop(tick)
-            ciclo = False  # Esce dal ciclo
-
-             
 
 
 def main():
-    home_screen()
-
+    start()
+   
 if __name__ == "__main__":
     main()
+
